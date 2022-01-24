dd60e196fdd5f8766223b2adf70818e315f214ea
Muhammad Ivan Maulana <36947684+ipengkah@users.noreply.github.com>
2022-01-24 10:59:56 +0700

    Add files via upload

---
+++ demo-api-crud/src/main/java/com/example/demo/DemoApiCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApiCrudApplication.class, args);
+	}
+
+}
---
+++ demo-api-crud/src/main/java/com/example/demo/dto/EmployeeDTO.java
@@ -0,0 +1,28 @@
+package com.example.demo.dto;
+
+public class EmployeeDTO {
+		
+	private long   employeeId;
+	private String employeeName;
+	private String employeeAddress;
+	public long getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(long employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public String getEmployeeAddress() {
+		return employeeAddress;
+	}
+	public void setEmployeeAddress(String employeeAddress) {
+		this.employeeAddress = employeeAddress;
+	}
+	
+	
+}
